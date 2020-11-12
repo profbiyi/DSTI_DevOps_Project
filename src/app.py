a7c6ea397ae523547a5792228bd774d4d81373ac
profbiyi <ahmed.oladapo@gmail.com>
2020-11-12 18:36:28 +0100

    Update app.py
    code clean up

--- src/app.py
+++ src/app.py
@@ -7,7 +7,6 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root_password@localhost:3307/test_db'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
