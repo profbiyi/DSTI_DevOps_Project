182a1003da005f3de67e13e7e51e4e46865b3879
profbiyi <ahmed.oladapo@gmail.com>
2020-11-13 13:59:27 +0100

    add test.py
    write test to ensure the api is working fine and well tested

---
+++ src/test.py
@@ -0,0 +1,32 @@
+try:
+        from app import app
+        import unittest
+except Exception as e:
+        print("Some Modules are missing {}".format(e))
+
+
+
+class FlaskTest (unittest.TestCase):
+    #check for response 200 on base entry page
+    def test_base(self):
+        tester = app.test_client(self)
+        response = tester.get("/")
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
+
+    def test_user_routes(self):
+        tester = app.test_client(self)
+        response = tester.get("/api/users")
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
+    
+    def test_user(self):
+        tester = app.test_client(self)
+        response = tester.get("/api/users")
+        self.assertEqual(response.content_type, "application/json")
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
